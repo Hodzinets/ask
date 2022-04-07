478457700fbb85accca4010ff8273e136f181ed7
Hodzinets <godzinets35@gmail.com>
2022-04-07 19:33:09 +0200

    Added keycloack and spring security configuration

--- ask/src/main/java/com/th/ask/controller/TestController.java
+++ ask/src/main/java/com/th/ask/controller/TestController.java
@@ -3,10 +3,19 @@ package com.th.ask.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 public class TestController {
     @GetMapping("/test")
-    public String index() {
+    @RolesAllowed("user")
+    public String test() {
         return "Hello ASK!";
+    }
+
+    @GetMapping("/test-admin")
+    @RolesAllowed("admin")
+    public String testAdmin() {
+        return "Hello ASK admin!";
     }
 }
